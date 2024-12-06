61a36f172bfdfb619c3555396e7436197c584939
NAVATEJA <navatejar80@gmail.com>
2024-12-06 14:14:52 +0530

    Add files via upload

--- pattern.java
+++ pattern.java
@@ -3,11 +3,11 @@ class Main {
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=0;i<=n;i++)
+        for(int i=n;i>=1;i--)
         {
-            for(int j=1;j<=n-i;j++)
+            for(int j=i;j>=1;j--)
             {
-                System.out.print("*");
+                System.out.print(j+" * ");
             }
             System.out.println();
 
